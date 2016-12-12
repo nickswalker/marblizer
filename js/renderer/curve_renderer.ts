ac02ab42f67fea04aeb1d59cc45b1abdadb3303b
Nick Walker <nick@nickwalker.us>
2016-12-12 03:06:31 -0600

    Add UI section of report

--- js/renderer/curve_renderer.ts
+++ js/renderer/curve_renderer.ts
@@ -36,7 +36,7 @@ class Drop {
 
     private static initialCirclePoints(radius: number, centerX: number, centerY: number) {
         let points: Array<Vec2> = [];
-        const desiredArcLength = 0.05;
+        const desiredArcLength = 0.01;
         const stepSize = desiredArcLength / radius;
         for (let i = 0.0; i < 2 * Math.PI; i += stepSize) {
             const newPoint = new Vec2(centerX + radius * Math.cos(i), centerY + radius * Math.sin(i));
